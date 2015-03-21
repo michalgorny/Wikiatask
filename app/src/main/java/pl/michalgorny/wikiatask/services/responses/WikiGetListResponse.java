51f8ca06be8cfe12a7e6b0bcab2232f521f821f8
michalgorny <michalgoorny@gmail.com>
2015-03-21 23:52:07 +0100

    Add Lombok library

--- app/src/main/java/pl/michalgorny/wikiatask/services/responses/WikiGetListResponse.java
+++ app/src/main/java/pl/michalgorny/wikiatask/services/responses/WikiGetListResponse.java
@@ -3,53 +3,17 @@ package pl.michalgorny.wikiatask.services.responses;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.Data;
+import lombok.experimental.Builder;
+
 /**
  *  Class corresponding response from retrieving wiki items
  */
+@Data
 public class WikiGetListResponse {
     private List<WikiItemResponse> items = new ArrayList<>();
     private Integer next;
     private Integer total;
     private Integer batches;
     private Integer currentBatch;
-
-    public List<WikiItemResponse> getItems() {
-        return items;
-    }
-
-    public void setItems(List<WikiItemResponse> items) {
-        this.items = items;
-    }
-
-    public Integer getNext() {
-        return next;
-    }
-
-    public void setNext(Integer next) {
-        this.next = next;
-    }
-
-    public Integer getTotal() {
-        return total;
-    }
-
-    public void setTotal(Integer total) {
-        this.total = total;
-    }
-
-    public Integer getBatches() {
-        return batches;
-    }
-
-    public void setBatches(Integer batches) {
-        this.batches = batches;
-    }
-
-    public Integer getCurrentBatch() {
-        return currentBatch;
-    }
-
-    public void setCurrentBatch(Integer currentBatch) {
-        this.currentBatch = currentBatch;
-    }
 }
--- app/src/main/java/pl/michalgorny/wikiatask/services/responses/WikiItemResponse.java
+++ app/src/main/java/pl/michalgorny/wikiatask/services/responses/WikiItemResponse.java
@@ -1,11 +1,12 @@
 package pl.michalgorny.wikiatask.services.responses;
 
 
-import android.util.Log;
+import lombok.Data;
 
 /**
  *  Class corresponding one item from response wiki list items
  */
+@Data
 public class WikiItemResponse {
     private Long id;
     private String name;
@@ -13,52 +14,4 @@ public class WikiItemResponse {
     private String language;
     private String topic;
     private String domain;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getHub() {
-        return hub;
-    }
-
-    public void setHub(String hub) {
-        this.hub = hub;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    public String getTopic() {
-        return topic;
-    }
-
-    public void setTopic(String topic) {
-        this.topic = topic;
-    }
-
-    public String getDomain() {
-        return domain;
-    }
-
-    public void setDomain(String domain) {
-        this.domain = domain;
-    }
 }
